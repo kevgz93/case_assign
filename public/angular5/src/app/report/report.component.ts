4d2650e92253ca20a9ce7ce0783ac1ad9dd4544a
kevin Guerrero <kguerreroz9364gmail.com>
2018-04-03 16:41:56 -0600

    send form with search doesn't work

--- public/angular5/src/app/report/report.component.ts
+++ public/angular5/src/app/report/report.component.ts
@@ -12,34 +12,37 @@ import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 export class ReportComponent implements OnInit {
 
   myform: FormGroup;
+  public users;
+  public months;
 
   constructor(private service: ApiService, private router:Router, private fb: FormBuilder) { }
 
   getUsers(){
-    console.log("entro para traerse los ususarios");
     this.service.getAllUsers()
     .subscribe(data => {
       console.log(data);
+      this.users = data;
     })
-
+    return;
   }
   
-  getTickets(){
-    this.service.getAllEngineers()
+  getTickets(form){
+    console.log(form);
+    /* this.service.getAllEngineers()
       .subscribe(data => {
 
-      })
+      }) */
   }
 
   ngOnInit() {
-    this.getUsers;
+    this.months = [{"id": 1, "name":"January"},{"id": 2, "name":"February"},{"id": 3, "name":"March"},{"id": 4, "name":"Abril"},
+    {"id": 5, "name":"May"},{"id": 6, "name":"June"},{"id": 7, "name":"July"},{"id": 8, "name":"August"},{"id": 9, "name":"September"},
+    {"id": 10, "name":"October"},{"id": 11, "name":"November"},{"id": 12, "name":"December"}];
+    this.getUsers();
     this.myform= this.fb.group({
       user: '',
       month: '',
-      year: '',
-      status: '',
-      start_day : '',
-      end_day: ''
+      case_status: '',
 
     });
   }
